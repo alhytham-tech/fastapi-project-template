3b987a306a0e2d469d3167c59a30cc0cde7fa993
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2021-04-01 21:43:42 +0100

    feat(routes): Add create_permission() route

--- fastapi-project-template/access_control/router.py
+++ fastapi-project-template/access_control/router.py
@@ -1,4 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import Session
+
+import dependencies as deps
+from access_control import cruds, models, schemas
 
 
 
@@ -7,3 +12,22 @@ perms_router = APIRouter(
     prefix='/permissions',
     tags=['Permissions']
 )
+
+
+@perms_router.post('',
+    status_code=201,
+    response_model=schemas.PermissionSchema
+)
+def create_permission(
+    perm_data: schemas.PermissionCreate,
+    dba: Session = Depends(deps.get_db)
+):
+    try:
+        permission = cruds.create_permission(db=dba, perm_data=perm_data)
+    except IntegrityError as e:
+        raise HTTPException(
+            status_code=400,
+            detail='This permission name is already in use'
+        )
+    else:
+        return permission
