766754df6438c667eb2d7c7981644b48989189ed
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2021-04-01 22:53:06 +0100

    refactor(models): Add Role model and its migration

--- fastapi-project-template/access_control/models.py
+++ fastapi-project-template/access_control/models.py
@@ -1,7 +1,21 @@
+from sqlalchemy import Column, ForeignKey, Integer, Table
+from sqlalchemy.orm import relationship
+
 from config.db import Base
 from mixins.columns import BaseMixin, BaseUACMixin
 
 
 
+# Many to Many associations
+permission_role = Table('permission_role', Base.metadata,
+    Column('permission_id', Integer, ForeignKey('permissions.id')),
+    Column('role_id', Integer, ForeignKey('roles.id'))
+)
+
+
 class Permission(BaseUACMixin, Base):
     pass
+
+
+class Role(BaseUACMixin, Base):
+    permissions = relationship("Permission", secondary=permission_role)
