dca727164bf619a4ca28023fa10cfc15473ab276
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2021-04-02 20:18:01 +0100

    feat(router): Add create_user() route

--- fastapi-project-template/users/router.py
+++ fastapi-project-template/users/router.py
@@ -1,4 +1,11 @@
-from fastapi import APIRouter
+from typing import List
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import Session
+
+import dependencies as deps
+from users import cruds, models, schemas
 
 
 
@@ -6,3 +13,22 @@ users_router = APIRouter(
     prefix='/users',
     tags=['User']
 )
+
+
+@users_router.post('',
+    status_code=201,
+    response_model=schemas.UserSchema
+)
+def create_user(
+    user_data: schemas.UserCreate,
+    dba: Session = Depends(deps.get_db)
+):
+    try:
+        user = cruds.create_user(db=dba, user_data=user_data)
+    except IntegrityError:
+        raise HTTPException(
+            status_code=403,
+            detail='This email is already in use'
+        )
+    return user
+
