c9eba24e4eb48ab2fa1819fcc78763d5968c6187
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2021-04-02 20:16:02 +0100

    chore(users): Add user related schemas

---
+++ fastapi-project-template/users/schemas.py
@@ -0,0 +1,38 @@
+from typing import List, Optional
+from pydantic import BaseModel, EmailStr
+
+from mixins.schemas import BaseSchemaMixin
+from access_control.schemas import GroupSchema
+
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    firstname: str
+    lastname: str
+    middlename: Optional[str]
+    password: str
+
+
+class UserSchema(BaseSchemaMixin):
+    email: EmailStr
+    firstname: str
+    lastname: str
+    middlename: str
+    is_active: bool
+    is_superuser: bool
+    group: GroupSchema = None
+
+    @property
+    def permissions(self) -> List[str]:
+        perms = []
+        for role in self.group.roles:
+            for perm in role.permissions:
+                perms.append(perm.name)
+        return perms
+
+    def has_permission(self, permission: str) -> bool:
+        return permission in self.permissions
+
+    class Config:
+        orm_mode = True
