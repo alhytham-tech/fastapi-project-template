bc66ecaf3627d37590e57fe888e3505358cf5607
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2021-04-03 22:59:11 +0100

    refactor(dependencies): Add get_current_user()

--- fastapi-project-template/dependencies.py
+++ fastapi-project-template/dependencies.py
@@ -1,5 +1,17 @@
+from jose import jwt
+from fastapi import Depends, HTTPException
+from sqlalchemy.orm import Session
+from decouple import config as decouple_config
+from fastapi.security import OAuth2PasswordBearer
+
 from config import db
 
+
+
+SECRET_KEY = decouple_config('SECRET_KEY')
+TOKEN_URL = decouple_config('DOCS_AUTH_URL', default='token')
+ALGORITHM = "HS256"
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl=TOKEN_URL)
 
 
 def get_db():
@@ -8,3 +20,11 @@ def get_db():
         yield dbase
     finally:
         dbase.close()
+
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    user = users_schema.UserSchema.from_orm(
+        users_cruds.get_user_by_email(db=db, email=payload['email'])
+    )
+    return user
