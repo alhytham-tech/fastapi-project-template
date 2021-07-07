e781019a6dbfb869e27b2643c6f858c56417a333
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2021-07-07 14:51:11 +0100

    chore: Delete db init files on successful initialization

--- {{cookiecutter.project_slug}}/app/initial_data.py
+++ {{cookiecutter.project_slug}}/app/initial_data.py
@@ -1,15 +1,13 @@
+import os
 import logging
+from sys import argv
 from sqlalchemy.exc import ProgrammingError
 
 from config.init_db import init_db
 from config.db import SessionLocal
 
 logging.basicConfig(level=logging.INFO)
-date_created = '''\n
-Use the info below to login:
-email: {{cookiecutter.superuser_email}}
-password: {{cookiecutter.superuser_password}}\n
-'''
+logger = logging.getLogger(' [FastAPI Template]')
 
 def init() -> None:
     db = SessionLocal()
@@ -18,15 +16,18 @@ def init() -> None:
 
 def main() -> None:
     try:
-        logger.info('Creating initial data')
+        logger.info(' Creating initial data...')
         init()
-        logger.info('Initial data created')
-        logger.info(date_created)
+        logger.info(' Initial data created.')
     except ProgrammingError:
         logger.error(
             ' Cannot find tables on your database. '
             'Have you ran migrations with "alembic upgrade head"?'
         )
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
+    # Auto delete this file 'app/initial_data.py' &
+    #'app/config/init_db.py' after use.
+    os.remove(os.path.join(os.getcwd(), 'config', 'init_db.py'))
+    os.remove(argv[0])
